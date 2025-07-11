9506d3771e507e90e22ca1ac129f3b90046f09e3
Tung Du <dinhtungdu@gmail.com>
2025-07-11 08:42:14 +0700

    style: fix linting issues in ProductFilterAttribute and QueryFilters classes

--- plugins/woocommerce/src/Blocks/BlockTypes/ProductFilterAttribute.php
+++ plugins/woocommerce/src/Blocks/BlockTypes/ProductFilterAttribute.php
@@ -110,7 +110,7 @@ final class ProductFilterAttribute extends AbstractBlock {
 				continue;
 			}
 
-			// Filter out empty slugs and trim whitespace
+			// Filter out empty slugs and trim whitespace.
 			$term_slugs = array_filter(
 				array_map( 'trim', explode( ',', $params[ $param_key ] ) ),
 			);
