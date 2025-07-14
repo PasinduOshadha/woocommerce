2a17424a8cd00d074858de72ca7087fb7db17e2e
Thomas Roberts <5656702+opr@users.noreply.github.com>
2025-07-14 16:06:58 +0100

    Add core profiler onboarding task

---
+++ plugins/woocommerce/src/Admin/Features/OnboardingTasks/Tasks/CoreProfiler.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Automattic\WooCommerce\Admin\Features\OnboardingTasks\Tasks;
+
+use Automattic\WooCommerce\Internal\Admin\Onboarding\OnboardingProfile;
+use Automattic\WooCommerce\Admin\Features\OnboardingTasks\Task;
+
+/**
+ * Core Profiler Task
+ */
+class CoreProfiler extends Task {
+	/**
+	 * ID.
+	 *
+	 * @return string
+	 */
+	public function get_id() {
+		return 'core_profiler';
+	}
+
+	/**
+	 * Title.
+	 *
+	 * @return string
+	 */
+	public function get_title() {
+		if ( $this->is_complete() ) {
+			return __( 'You completed the store setup', 'woocommerce' );
+		}
+		return __( 'Store setup', 'woocommerce' );
+	}
+
+	/**
+	 * Content.
+	 *
+	 * @return string
+	 */
+	public function get_content() {
+		return __(
+			'Share a few details about your store to help us provide the best setup experience for you.',
+			'woocommerce'
+		);
+	}
+
+	/**
+	 * Time.
+	 *
+	 * @return string
+	 */
+	public function get_time() {
+		return __( '2 minutes', 'woocommerce' );
+	}
+
+	/**
+	 * Action URL.
+	 *
+	 * @return string
+	 */
+	public function get_action_url() {
+		return admin_url( 'admin.php?page=wc-admin&path=%2Fsetup-wizard' );
+	}
+
+	/**
+	 * Task completion.
+	 *
+	 * @return bool
+	 */
+	public function is_complete() {
+		return ! OnboardingProfile::needs_completion();
+	}
+
+	/**
+	 * Task visibility.
+	 *
+	 * @return bool
+	 */
+	public function can_view() {
+		return class_exists( 'Automattic\WooCommerce\Internal\Admin\Onboarding\OnboardingProfile' );
+	}
+}
